c6413b521be52c026ae3ebe223ccc229d183f415
Angelina Olmedo <angelinaolmedo7@gmail.com>
2019-02-01 11:12:47 -0800

    Unit 02 (unfinished)

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Angelina Olmedo
+//Date - 02/01/2019
+//Class - APCSA
+//Lab  - Trapezoid Area (Trap & TrapRunner)
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return ((base1+base2)*.5)*height;
+	}
+}
