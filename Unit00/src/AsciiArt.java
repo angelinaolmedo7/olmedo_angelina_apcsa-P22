7ec4fcb908dc09c9ab7deeb5e21dd165747c3bd8
Angelina Olmedo <angelinaolmedo7@gmail.com>
2019-02-01 10:20:59 -0800

    Unit00 (unfinished)

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -Angelina Olmedo
+//Date - 01/31/2019
+//Class - APCSA
+//Lab  - AsciiArt
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Angelina Olmedo \n\n" );
+		System.out.println("Gnome" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                /\\                " );
+		System.out.println("               /  \\               " );
+		System.out.println("              /    \\              " );
+		System.out.println("             --------              " );
+		System.out.println("             ( *   * )             " );
+		System.out.println("             |    ^   )            " );
+		System.out.println("              \\___w__)             " );
+		System.out.println("            [          ]           " );
+		System.out.println("            |  |     | |           " );
+		System.out.println("            |  |     | |           " );
+		System.out.println("            |  |     | |           " );
+		System.out.println("              \\       /            " );
+		System.out.println("               |  |  |             " );
+		System.out.println("               |__|__|             " );
+		System.out.println("              (___|___)            " );
+		//add other output
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,35 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Angelina Olmedo
+//Date -1/31/19
+//Class - APCSA
+//Lab  - 1 AsciiBox
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Angelina Olmedo \t  01/31/2019 \n\n" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("EEEEEEEEEEEEEEEEEEEEEEEEE" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("ooooooooooooooOOoooooOooo" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++++++++++++++++++++++++" );
+
+	}
+}
