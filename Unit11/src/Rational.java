6cb55a454146790f8952797218ecd2de7928e5e2
Angelina Olmedo <angelinaolmedo7@gmail.com>
2019-03-14 11:09:01 -0700

    unit 11 unfinished

--- Unit11/src/Rational.java
+++ Unit11/src/Rational.java
@@ -3,70 +3,100 @@
 //Name -
 
 import static java.lang.System.*; 
+import java.math.BigInteger;
 
 class Rational implements Comparable<Rational>
 {
-	//add two instance variables
+	private int numerator, denominator;
 
-	//write two constructors
-
-
-	//write a setRational method
+	public Rational () {
+		setRational(0,1);
+	}
 	
+	public Rational (int num, int den) {
+		setRational(num,den);
+	}
 
-	//write  a set method for numerator and denominator
-
+	public void setRational(int num, int den) {
+		setNumerator(num);
+		setDenominator(den);
+	}
+	
+	public void setNumerator(int num) {
+		numerator = num;
+	}
+	
+	public void setDenominator(int den) {
+		denominator = den;
+	}
 	
 	public void add(Rational  other)
 	{
 		//num1/den1 + num2/den2 
-		//new numerator = (num1 * den2 + num2 * den1)
-		//new denominator = (den1 * den2)
-		
-		
-
+		numerator = (numerator * other.getDenominator() + other.getNumerator() * denominator);
+		denominator = (denominator * other.getDenominator());
 
 		reduce();
 	}
 
 	private void reduce()
 	{
-
-
-
+		int grcode = gcd(numerator, denominator);
+		setNumerator(numerator/grcode); 
+		setDenominator(denominator/grcode);
+	}
+	
+	private Rational tempReduce(int num, int den)
+	{
+		int grcode = gcd(num, den);
+		return new Rational (num/grcode, den/grcode);
 	}
 
 	private int gcd(int numOne, int numTwo)
 	{
-
-
-		return 1;
+		BigInteger numer = new BigInteger(Integer.toString(numOne));
+		BigInteger denom = new BigInteger(Integer.toString(numTwo));
+		
+		return numer.gcd(denom).intValue();
 	}
 
-	public Object clone ()
+	public Rational clone ()
 	{
-		return "";
+		return new Rational(numerator,denominator);
 	}
 
 
 	//ACCESSORS
 
-	//write get methods for numerator and denominator
+	public int getNumerator() {
+		return numerator;
+	}
 	
+	public int getDenominator() {
+		return denominator;
+	}
 	
-	public boolean equals( Object obj)
+	public boolean equals( Rational obj)
 	{
-
-
-		return false;
+		if ().) {
+			return true;
+		}
+		else return false;
 	}
 
 	public int compareTo(Rational other)
 	{
-
-
-		return -1;
-	}
+		double first = numerator/denominator;
+		double second = other.getNumerator()/other.getDenominator();
+		
+		if (first>second) {
+			return 1;
+		}
+		else if (first==second) {
+			return 0;
+		}
+		else return -1;
+	}   //i'm not entirely sure what I was supposed to do here
 
 
 
