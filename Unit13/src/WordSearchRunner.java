87b4f88a8cb5c95876eecf6be722f2571de714e9
Angelina <angelinaolmedo7@gmail.com>
2019-04-02 19:41:55 -0700

    unit 13 finished

--- Unit13/src/WordSearchRunner.java
+++ Unit13/src/WordSearchRunner.java
@@ -10,7 +10,7 @@ public class WordSearchRunner
     public static void main(String[] args) throws Exception
     {
     	  WordSearch ms = new WordSearch(8,"APPLEXYPXLHJKEEDEGGLLXXCGFPDGOGNMYNTAHUUPUQDGBTSBTHIGHMSILKXLTHIS");
-    	  String[] words = "APPLE AXE APEX CAT HEX EGG HAT COMPUTER GUM THIS TUG THIGH ELP KTP XEC".split(" ");
+    	  String[] words = "APPLE AXE APEX CAT HEX EGG HAT COMPUTER GUM THIS TUG THIGH ELP KTP XEE XEC".split(" ");
     	  System.out.println( ms );
     	  for( String s : words )
     	  {
